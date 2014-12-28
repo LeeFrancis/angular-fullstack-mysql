a10d5a822209fa734d083445bcfe68d5e8210043
Lee Francis <lee.x.francis@gmail.com>
2014-12-27 23:02:00 -0500

    Update development.js

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -3,14 +3,10 @@
 // Development specific configuration
 // ==================================
 module.exports = {
-  // MongoDB connection options
-  mongo: {
-    uri: 'mongodb://localhost/beerigo-dev'
-  },
   mysql: {
-    username: 'beerigo',
-    password: 'beer1g0',
-    database: 'beerigo_dev',
+    username: '',
+    password: '',
+    database: '',
     host: '127.0.0.1',
     dialect: 'mysql'
   },  
