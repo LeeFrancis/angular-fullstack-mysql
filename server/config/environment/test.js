31a9226f64a2369710a31d7faa743cb861f1cefc
Lee Francis <lee.x.francis@gmail.com>
2014-12-27 23:03:18 -0500

    Update test.js

--- server/config/environment/test.js
+++ server/config/environment/test.js
@@ -3,14 +3,10 @@
 // Test specific configuration
 // ===========================
 module.exports = {
-  // MongoDB connection options
-  mongo: {
-    uri: 'mongodb://localhost/beerigo-test'
-  },
   mysql: {
-    username: 'beerigo',
-    password: 'beer1g0',
-    database: 'beerigo_dev',
+    username: '',
+    password: '',
+    database: '',
     host: '127.0.0.1',
     dialect: 'mysql'
   }  
